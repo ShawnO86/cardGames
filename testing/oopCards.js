5703760cea31f42d852af97c33dfb01e2685ad2d
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-02-08 20:17:12 -0600

    cleaning up render function

--- testing/oopCards.js
+++ testing/oopCards.js
@@ -90,25 +90,29 @@ class Table {
     }
 
     renderHand(hand, playerName) {
-        const handHolder = document.createElement("div");
-        document.body.appendChild(handHolder);
-        handHolder.classList.add("hand", playerName);// this.players);
-        console.log(hand.length, playerName)
+        console.log(gameArea.children)
+        console.log('renderHand data:', hand.length, playerName, hand)
+        const dealerHandHolder = document.querySelector('.dealerHand')
+        const playerHandHolder = document.querySelector('.playerHand')
+
         for (let i = 0; i < hand.length; i++) {
-            const card = document.createElement("div");
+
             const cardHolder = document.createElement("div");
             const button = document.createElement("button");
-
-            button.innerHTML = "New Card";
-            button.classList.add("newCard");
+            const card = document.createElement("div");
             let cardSuit = hand[i].suit;
             let cardFace = hand[i].rank;
+            button.innerHTML = "New Card";
+            button.classList.add("newCard", cardSuit + cardFace, playerName);
+
             console.log(cardSuit, cardFace)
             cardHolder.classList.add("cardHolder");
             card.classList.remove('H', 'D', 'S', 'C', 'A', 'K', 'Q', 'J');
-            card.classList.add("card", cardSuit, 'in_animation');
-            button.classList.add(cardSuit + cardFace, playerName)
+            card.classList.add("card", 'in_animation');
+
             if (cardFace === 'A' || cardFace === 'K' || cardFace === 'Q' || cardFace === 'J') {
+                card.classList.add(cardSuit, cardFace)
+                //face card
                 card.innerHTML =
                     `<div class="card-value-suit top"> <span>${cardFace}</span> <span>${this.suitIcon(cardSuit)}</span></div>
                 <div class="card-suit ${cardFace}"> ${cardFace} </div>
@@ -116,63 +120,60 @@ class Table {
                 cardHolder.appendChild(card);
             } else {
                 const suitDisp = document.createElement("div");
+                card.classList.add(cardSuit, cardFace)
                 suitDisp.classList.add("card-suit", cardFace);
                 //top of card
                 card.innerHTML = `<div class="card-value-suit top"> <span>${cardFace}</span> <span>${this.suitIcon(cardSuit)}</span></div>`;
+                
                 //add icons to icon holder div based on card value
                 for (let i = 0; i < Number(cardFace); i++) {
                     suitDisp.innerHTML += `<span>${this.suitIcon(cardSuit)}</span>`;
                 };
+
                 //append icons to middle of card
                 card.appendChild(suitDisp);
                 //bottom of card
                 card.innerHTML += `<div class="card-value-suit bot"> <span>${cardFace}</span> <span>${this.suitIcon(cardSuit)}</span></div>`;
                 cardHolder.appendChild(card)
             }
+
+            //need new card function here
             cardHolder.appendChild(button);
-            handHolder.appendChild(cardHolder)
+            button.addEventListener('click', () => {
+                //gameArea.innerHTML = ''
+                console.log(button.classList[1], "clicked")
+            })
+
+            //append card to holder
+            if (playerName === "Dealer") {
+                dealerHandHolder.appendChild(cardHolder)
+            }
+            else {
+                playerHandHolder.appendChild(cardHolder)
+            }
         }
-    }
-    getNewCard(hand) {
-        let deck = this.deck.cards;
-        console.log(hand)
-       // hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0])
     }
 
 }
 
-let gameBoard = new Table();
-//const main = document.createElement('main');
-const dealButton = document.querySelector('.deal');
-
-let playerHand = [];
-let dealherHand = []
-let total;
-dealButton.addEventListener("click", () => {
-    // document.appendChild(main)
-    gameBoard.start('Player', 'Dealer');
-    playerHand = gameBoard.players[0].playerCards;
-    dealerHand = gameBoard.players[1].playerCards
-    gameBoard.renderHand(playerHand, gameBoard.players[0].playerName)
-    gameBoard.renderHand(dealerHand, gameBoard.players[1].playerName)
-    //total = gameBoard.calcTotal(playerHand)
-    console.log(gameBoard)
-    console.log(gameBoard.players[0].playerCards, gameBoard.players[0].playerName);
-    console.log(gameBoard.players[1].playerCards, gameBoard.players[1].playerName);
-    console.log(gameBoard.deck.cards)
-    console.log('calc total: ',gameBoard.calcTotal(playerHand))
-    let newCardBtns = document.querySelectorAll('.newCard')
-    console.log(newCardBtns)
-    newCardBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            let player = btn.classList[2].toLowerCase();
-            console.log(btn.classList[1])
-            console.log(player)
-            console.log(gameBoard.deck)
-            gameBoard.getNewCard(player + 'hand')
-        })
-
-    });
-})
+const gameArea = document.querySelector(".gameArea");
+function main() {
+    let gameBoard = new Table();
+    const dealButton = document.querySelector('.deal');
 
 
+    let playerHand = [];
+    let dealerHand = []
+    dealButton.addEventListener("click", () => {
+        //start match
+        gameBoard.start('Player', 'Dealer');
+        //define starting hands
+        playerHand = gameBoard.players[0].playerCards;
+        dealerHand = gameBoard.players[1].playerCards;
+        //render hands to browser
+        gameBoard.renderHand(playerHand, gameBoard.players[0].playerName);
+        gameBoard.renderHand(dealerHand, gameBoard.players[1].playerName);
+    })
+}
+
+main()
